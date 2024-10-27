bd8b55aaa11e7d8726ffaee113e92080e92c6f21
Alexander1465 <alexandervecin@gmail.com>
2024-10-27 20:45:07 +0000

    Proyecto

--- src/components/button/button.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import Button from '@mui/material/Button';
-function CustomButton(props) {
-    var bgcolor = props.bgcolor, text = props.text, txtcolor = props.txtcolor, disabled = props.disabled, onClick = props.onClick, size = props.size;
-    return (
-    _jsx(Button, { type: 'button', onClick: onClick, disabled: disabled, size: size, sx: { backgroundColor: bgcolor, color: txtcolor }, children: text }));
-}
-export default CustomButton;
